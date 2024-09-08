c51718a80997b957d5143eb09a5f0015735ef43d
H82M19\Student <Student>
2024-09-08 10:25:00 +0700

    week2 js exDatastructer bai1 #6#7

--- DataStructure/CodingChallenge1.js
+++ DataStructure/CodingChallenge1.js
@@ -24,11 +24,58 @@
 
 
 
-const players1 = ['Neuer', 'Pavard', 'Boateng', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Muller', 'Gnabry', 'Lewandowski', 'Coman'];
-const players2 = ['Burki', 'Piszczek', 'Hummels', 'Akanji', 'Hakimi', 'Weigl', 'Brandt', 'Hazard', 'Sancho', 'Reus', 'Gotze'];
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+    [
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnarby',
+    'Lewandowski',
+    ],
+    [
+    'Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+    ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    },
+    };
 
+//  1. Create one player array for each team (variables 'players1' and 'players2')
+const players1 = game.players[0]; 
+const players2 = game.players[1];
 
-const gk = players1[0]; 
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+
+const gk = players1[0];  // Goalkeeper
 const fieldPlayers = players1.slice(1);
 
 // 3. Create an array 'allPlayers' containing all players of both teams (22 players)
@@ -41,13 +88,7 @@ const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
 
 // 5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
 
-const game = {
-    odds: {
-        team1: 1.33,
-        x: 3.25,
-        team2: 6.5
-    }
-}
+const {team1, x: draw, team2} = game.odds;
 
 // 6. Write a function ('printGoals') that receives an arbitrary number of player names (not an array) and prints each of them to the console, 
 // along with the number of goals that were scored in total (number of player names passed in)
@@ -62,3 +103,11 @@ function  printGoals(...players) {
 // printGoals('Davies', 'Muller', 'Sancho', 'Kimmich');
 
 printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich')
+printGoals("\n Game scored : " ,...game.scored);
+
+
+// 7. The team with the lower odd is more likely to win. 
+// Print to the console which team is more likely to win, without using an if/else statement or the ternary operator.
+
+game.odds.team1 < game.odds.team2 && console.log('\n Team 1 is more likely to win');
+game.odds.team1 > game.odds.team2 && console.log('\n Team 2 is more likely to win');
