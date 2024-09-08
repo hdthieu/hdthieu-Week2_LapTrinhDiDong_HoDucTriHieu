143096a3119a93907ef8b27348bd1b8fc606ad02
H82M19\Student <Student>
2024-09-08 11:20:36 +0700

    week2 js exDatastructer bai2 #3 #4

--- DataStructure/CodingChallenge2.js
+++ DataStructure/CodingChallenge2.js
@@ -79,7 +79,7 @@ const game = {
         sum += odd;
     })
 
-    console.log(`Average odd: ${sum / Object.values(game.odds).length}`);
+    console.log(`\nAverage odd: ${sum / Object.values(game.odds).length}`);
 
 
 // 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
@@ -88,5 +88,27 @@ const game = {
 //      Odd of victory Borrussia Dortmund: 6.5
 //  Get the team names directly from the game object, don't hardcode them (except for "draw").
 //  Hint: Note how the odds and the game objects have the same property names 
+
+const {team1, odds, team2} = game;
+
+console.log(`\nOdd of victory ${game.team1}: ${odds.team1}`);
+console.log(`Odd of draw  ${odds.x}`);
+console.log(`Odd of victory ${game.team2}: ${odds.team2}`);
+
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. 
+// In this game, it will look like this:
+//  {
+//      Gnarby: 1,
+//      Hummels: 1,
+//      Lewandowski: 2
+//  }
+
+const scorers = {};
+const count = 0;
+game.scored.forEach((player) => {
+   scorers[player] = (scorers[player] || 0) + 1;
+})
+console.log(scorers);
 
 
